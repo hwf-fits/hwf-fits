36365c29d3bde647c5571964ede967d54f5c7b17
Silverio <gsilver@umich.edu>
2018-03-01 13:12:05 -0500

    commments, readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hwf-fits
+
+Deploy to web server.
+
+For development, run `python -m SimpleHTTPServer 8000` in the root of the source folder.
--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,16 @@
 fitsApp.controller('fitsController', ['$rootScope', '$scope', '$filter', '$timeout', '$log', 'getStuff', function($rootScope, $scope, $filter, $timeout, $log, getStuff) {
+  $scope.downloadedICS=false;
   $scope.lookUpPerson = function(instructorInput){
     getStuff.getGenericStuff('data/' + instructorInput + '.json').then(function(personData) {
       $log.warn(personData.data.getMyClsScheduleResponse.RegisteredClasses);
       $scope.classList = personData.data.getMyClsScheduleResponse.RegisteredClasses;
+    });
+  };
+  $scope.downloadICS = function(instructorInput){
+    // may need to change this url!
+    getStuff.getGenericStuff('dir_where_ics_can_be_found/' + instructorInput + '.ics').then(function(ics) {
+      $scope.downloadedICS = true;
+      $scope.ICS = ics.data;
     });
   };
 }]);
